82f56d306e293948c768f1e633ba543b90276f6e
Ivan Markovski <sagitariusim@gmail.com>
2025-02-06 22:02:25 +0100

    Adding alert dialog for position details button

--- app/src/main/java/com/example/scheduleappui/ui/theme/Positions.kt
+++ app/src/main/java/com/example/scheduleappui/ui/theme/Positions.kt
@@ -1,8 +1,6 @@
 package com.example.scheduleappui.ui.theme
 
-import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,41 +14,33 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
+import androidx.compose.material3.Button
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Card
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Divider
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-//noinspection UsingMaterialAndMaterial3Libraries
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.primarySurface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.scheduleappui.PositionViewModel
 import com.example.scheduleappui.screen.PositionItem
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun Positions() {
@@ -121,7 +111,7 @@ fun AlertDialogPosition(positionDetails : String, onDismiss: () -> Unit) {
                 }
             },
             modifier = Modifier.fillMaxWidth().
-            background(MaterialTheme.colors.primarySurface).padding(8.dp),
+            background(MaterialTheme.colorScheme.background).padding(8.dp),
             shape = RoundedCornerShape(5.dp),
             backgroundColor = Color.White,
             properties = DialogProperties(
